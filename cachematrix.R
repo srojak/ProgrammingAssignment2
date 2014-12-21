91fdd794905eb73ca82afe23e720067763019bf9
srojak <srojak@srojak.com>
2014-12-21 11:16:15 -0600

    Assignment 2
    Completed assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+## R Programming Assignment 2
+## stephen rojak
+## 21 Dec 14
 
-## Write a short comment describing this function
+## store a matrix with extended properties:
+##  a cacheable inverse
+##  a flag that indicates to called functions whether or not
+##    to message their actions while performing them
 
 makeCacheMatrix <- function(x = matrix()) {
-
+	## inverse stored here
+	inv <- NULL
+	## message control
+	is.noisy <- FALSE
+	set <- function(m) {
+		x <<- m
+		inv <<- NULL
+	}
+	get <- function() x
+	## manage cached inverse
+	setInverse <- function(inverse) inv <<- inverse
+	getInverse <- function() inv
+	## control messsage flag
+	setNoisy <- function(flag) is.noisy <<- flag
+	getNoisy <- function() is.noisy
+	list(set = set, get = get,
+		setInverse = setInverse,
+		getInverse = getInverse,
+		setNoisy = setNoisy,
+		getNoisy = getNoisy)
 }
 
 
-## Write a short comment describing this function
+## Operating on a matrix stored by makeCacheMatrix,
+##  get the cached inverse if it exists
+##  or create it if it does not
+## x must have been created by makeCacheMatrix
+## other arguments are only used if cached inverse does not exist
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+		im <- x$getInverse()
+		if (!is.null(im)) {
+			if (x$getNoisy())
+				message("getting cached inverse");
+			return (im)
+		}
+		if (x$getNoisy())
+			message("computing inverse");
+		mat <- x$get()
+		im <- solve(mat, ...)
+		x$setInverse(im)
+		im
 }
